c21dc56c8958887d335bd0f40d59ead1d85f768d
SmokedKoala <k.azarov@inbox.ru>
2020-11-13 18:16:58 +0300

    markdown page and slider on main page

--- myTry/js/script.js
+++ myTry/js/script.js
@@ -1,9 +1,35 @@
 // функция обработки нажатия на иконку меню
 menu.onclick = function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += "  responsive";
-    } else {
-        x.className = "topnav";
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += "  responsive";
+        } else {
+            x.className = "topnav";
+        }
     }
+    // функционал карусели на главном экране
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var slides = document.getElementsByClassName("mySlides");
+    var i;
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
 }
